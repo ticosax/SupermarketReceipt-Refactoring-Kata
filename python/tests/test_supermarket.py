d69f6f1a586b913b9a4b998f761daa81b8eb3d51
Nicolas Delaby <nicolas.delaby@infarm.com>
2023-07-19 13:45:35 +0200

    96% coverage

--- python/tests/test_supermarket.py
+++ python/tests/test_supermarket.py
@@ -38,3 +38,21 @@ def test_3_for_2_discount(catalog, toothbrush):
     assert receipt_item.price == 99
     assert receipt_item.total_price == 99 * 4
     assert receipt_item.quantity == 4
+
+
+def test_5_for_amount_discount(catalog, toothbrush):
+    teller = Teller(catalog)
+    teller.add_special_offer(
+        SpecialOfferType.FIVE_FOR_AMOUNT, toothbrush, 400
+    )
+    cart = ShoppingCart()
+    cart.add_item_quantity(toothbrush, 6)
+    receipt = teller.checks_out_articles_from(cart)
+    assert receipt.total_price() == 400 + 99
+    assert receipt.discounts == [Discount(toothbrush, "5 for 400", -95)]
+    assert len(receipt.items) == 1
+    receipt_item = receipt.items[0]
+    assert receipt_item.product == toothbrush
+    assert receipt_item.price == 99
+    assert receipt_item.total_price == 594
+    assert receipt_item.quantity == 6
