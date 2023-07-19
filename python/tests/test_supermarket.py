58a73c4abfecfd1b4b63678805901c768309f3e9
Nicolas Delaby <nicolas.delaby@infarm.com>
2023-07-19 12:24:28 +0200

    Goes to 95% coverage

--- python/tests/test_supermarket.py
+++ python/tests/test_supermarket.py
@@ -1,4 +1,4 @@
-from model_objects import SpecialOfferType
+from model_objects import Discount, SpecialOfferType
 from shopping_cart import ShoppingCart
 from teller import Teller
 
@@ -20,3 +20,21 @@ def test_ten_percent_discount(catalog, toothbrush, apples):
     assert receipt_item.price == 199
     assert receipt_item.total_price == 199 * 2.5
     assert receipt_item.quantity == 2.5
+
+
+def test_3_for_2_discount(catalog, toothbrush):
+    teller = Teller(catalog)
+    teller.add_special_offer(
+        SpecialOfferType.THREE_FOR_TWO, toothbrush, catalog.unit_price(toothbrush)
+    )
+    cart = ShoppingCart()
+    cart.add_item_quantity(toothbrush, 4)
+    receipt = teller.checks_out_articles_from(cart)
+    assert receipt.total_price() == 99 * 3
+    assert receipt.discounts == [Discount(toothbrush, "3 for 2", -99)]
+    assert len(receipt.items) == 1
+    receipt_item = receipt.items[0]
+    assert receipt_item.product == toothbrush
+    assert receipt_item.price == 99
+    assert receipt_item.total_price == 99 * 4
+    assert receipt_item.quantity == 4
